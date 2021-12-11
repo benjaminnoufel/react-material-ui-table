d49fb86678d1ff21eba5b0a15c8d3e7305622dd3
Benjamin Noufel <benjamin.noufel@gmail.com>
2021-12-11 14:20:32 +0100

    :rotating_light: Fix linter

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,28 @@
-import commonjs from "@rollup/plugin-commonjs";
-import nodeResolve from "@rollup/plugin-node-resolve";
-import remove from "rollup-plugin-delete";
-import {terser} from "rollup-plugin-terser";
-import typescript from "@rollup/plugin-typescript";
+import commonjs from '@rollup/plugin-commonjs'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import remove from 'rollup-plugin-delete'
+import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript'
 
 export default {
-    input: "src/ReactMaterialUiTable.tsx",
-    plugins: [
-        remove({targets: "dist/*"}),
-        typescript(),
-        commonjs(),
-        nodeResolve(),
-        terser()
-    ],
-    external: [
-        "@material-ui",
-        "@material-ui/core/Table",
-        "@material-ui/core/TableHead",
-        "@material-ui/core/TablePagination",
-        "@material-ui/core/TableRow",
-        "@material-ui/core/OutlinedInput",
-        "@material-ui/core/TableCell",
-        "@material-ui/core/TableContainer",
-        "@material-ui/core",
-        "@material-ui/core/styles/makeStyles",
-        "react",
-        "react-dom"
-    ],
-    output: {
-        file: "dist/ReactMaterialUiTable.js",
-        format: "cjs"
-    }
-};
+  input: 'src/ReactMaterialUiTable.tsx',
+  plugins: [remove({ targets: 'dist/*' }), typescript(), commonjs(), nodeResolve(), terser()],
+  external: [
+    '@material-ui',
+    '@material-ui/core/Table',
+    '@material-ui/core/TableHead',
+    '@material-ui/core/TablePagination',
+    '@material-ui/core/TableRow',
+    '@material-ui/core/OutlinedInput',
+    '@material-ui/core/TableCell',
+    '@material-ui/core/TableContainer',
+    '@material-ui/core',
+    '@material-ui/core/styles/makeStyles',
+    'react',
+    'react-dom',
+  ],
+  output: {
+    file: 'dist/ReactMaterialUiTable.js',
+    format: 'cjs',
+  },
+}
--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,98 +1,114 @@
-import {entriesNotIn, objectByString, reorderFromObject} from "./utils";
+import { entriesNotIn, objectByString, reorderFromObject } from './utils'
 
-describe("test utils for Components", (): void => {
-    describe("test entriesNotIn", (): void => {
-        it("should be test entriesNotIn return header without filter", () => {
-            expect.assertions(1);
-            const headers: Record<string, string> = {name: "Nom", description: "Description"};
-            const data = Object
-                .entries(headers)
-                .filter(entriesNotIn(["a"]))
-                .map(([, headerCell]: [string, string]) => headerCell);
-            expect(data).toStrictEqual(["Nom", "Description"]);
-        });
+describe('test utils for Components', (): void => {
+  describe('test entriesNotIn', (): void => {
+    it('should be test entriesNotIn return header without filter', () => {
+      expect.assertions(1)
+      const headers: Record<string, string> = { name: 'Nom', description: 'Description' }
+      const data = Object.entries(headers)
+        .filter(entriesNotIn(['a']))
+        .map(([, headerCell]: [string, string]) => headerCell)
+      expect(data).toStrictEqual(['Nom', 'Description'])
+    })
 
-        it("should be test entriesNotIn return header with filter", () => {
-            expect.assertions(1);
-            const headers: Record<string, string> = {name: "Nom", description: "Description", filter: "ToFilter"};
-            const data = Object
-                .entries(headers)
-                .filter(entriesNotIn(["filter"]))
-                .map(([, headerCell]: [string, string]) => headerCell);
-            expect(data).toStrictEqual(["Nom", "Description"]);
-        });
-    });
+    it('should be test entriesNotIn return header with filter', () => {
+      expect.assertions(1)
+      const headers: Record<string, string> = {
+        name: 'Nom',
+        description: 'Description',
+        filter: 'ToFilter',
+      }
+      const data = Object.entries(headers)
+        .filter(entriesNotIn(['filter']))
+        .map(([, headerCell]: [string, string]) => headerCell)
+      expect(data).toStrictEqual(['Nom', 'Description'])
+    })
+  })
 
-    describe("test reorderFromObject", (): void => {
-        it("should be have no entries", () => {
-            expect.assertions(2);
-            const headers: Record<string, string> = {};
-            const row = {name: "Product 1", description: "Description of product one"};
-            const data = Object.entries(reorderFromObject(headers, row));
-            expect(data).toHaveLength(0);
-            expect(data).toStrictEqual([]);
-        });
+  describe('test reorderFromObject', (): void => {
+    it('should be have no entries', () => {
+      expect.assertions(2)
+      const headers: Record<string, string> = {}
+      const row = { name: 'Product 1', description: 'Description of product one' }
+      const data = Object.entries(reorderFromObject(headers, row))
+      expect(data).toHaveLength(0)
+      expect(data).toStrictEqual([])
+    })
 
-        it("should be have entries but don't have value", () => {
-            expect.assertions(10);
-            const headers: Record<string, string> = {name: "Nom", description: "Description", filter: "ToFilter"};
-            const row = {};
-            const data = Object.entries(reorderFromObject(headers, row));
-            expect(data).toHaveLength(3);
-            expect(data[0]).toHaveLength(2);
-            expect(data[0][0]).toStrictEqual("name");
-            expect(data[0][1]).toBeUndefined();
+    it("should be have entries but don't have value", () => {
+      expect.assertions(10)
+      const headers: Record<string, string> = {
+        name: 'Nom',
+        description: 'Description',
+        filter: 'ToFilter',
+      }
+      const row = {}
+      const data = Object.entries(reorderFromObject(headers, row))
+      expect(data).toHaveLength(3)
+      expect(data[0]).toHaveLength(2)
+      expect(data[0][0]).toStrictEqual('name')
+      expect(data[0][1]).toBeUndefined()
 
-            expect(data[1]).toHaveLength(2);
-            expect(data[1][0]).toStrictEqual("description");
-            expect(data[1][1]).toBeUndefined();
+      expect(data[1]).toHaveLength(2)
+      expect(data[1][0]).toStrictEqual('description')
+      expect(data[1][1]).toBeUndefined()
 
-            expect(data[2]).toHaveLength(2);
-            expect(data[2][0]).toStrictEqual("filter");
-            expect(data[2][1]).toBeUndefined();
-        });
+      expect(data[2]).toHaveLength(2)
+      expect(data[2][0]).toStrictEqual('filter')
+      expect(data[2][1]).toBeUndefined()
+    })
 
-        it("should be have a valid entries", () => {
-            expect.assertions(10);
-            const headers: Record<string, string> = {name: "Nom", description: "Description", filter: "ToFilter"};
-            const row = {name: "Product 1", description: "Description of product one"};
-            const data = Object.entries(reorderFromObject(headers, row));
-            expect(data).toHaveLength(3);
-            expect(data[0]).toHaveLength(2);
-            expect(data[0][0]).toStrictEqual("name");
-            expect(data[0][1]).toStrictEqual("Product 1");
+    it('should be have a valid entries', () => {
+      expect.assertions(10)
+      const headers: Record<string, string> = {
+        name: 'Nom',
+        description: 'Description',
+        filter: 'ToFilter',
+      }
+      const row = { name: 'Product 1', description: 'Description of product one' }
+      const data = Object.entries(reorderFromObject(headers, row))
+      expect(data).toHaveLength(3)
+      expect(data[0]).toHaveLength(2)
+      expect(data[0][0]).toStrictEqual('name')
+      expect(data[0][1]).toStrictEqual('Product 1')
 
-            expect(data[1]).toHaveLength(2);
-            expect(data[1][0]).toStrictEqual("description");
-            expect(data[1][1]).toStrictEqual("Description of product one");
+      expect(data[1]).toHaveLength(2)
+      expect(data[1][0]).toStrictEqual('description')
+      expect(data[1][1]).toStrictEqual('Description of product one')
 
-            expect(data[2]).toHaveLength(2);
-            expect(data[2][0]).toStrictEqual("filter");
-            expect(data[2][1]).toBeUndefined();
-        });
-    });
+      expect(data[2]).toHaveLength(2)
+      expect(data[2][0]).toStrictEqual('filter')
+      expect(data[2][1]).toBeUndefined()
+    })
+  })
 
-    describe("test objectByString", (): void => {
-        it("should be have a result without search", () => {
-            expect.assertions(5);
-            const rows = [{name: "Product 1", description: "Description of product one"}, {name: "Product 2", description: "Description of product two"}];
-            const data = rows.filter(objectByString(null));
-            expect(data).toHaveLength(2);
+  describe('test objectByString', (): void => {
+    it('should be have a result without search', () => {
+      expect.assertions(5)
+      const rows = [
+        { name: 'Product 1', description: 'Description of product one' },
+        { name: 'Product 2', description: 'Description of product two' },
+      ]
+      const data = rows.filter(objectByString(null))
+      expect(data).toHaveLength(2)
 
-            expect(data[0].name).toStrictEqual("Product 1");
-            expect(data[0].description).toStrictEqual("Description of product one");
+      expect(data[0].name).toStrictEqual('Product 1')
+      expect(data[0].description).toStrictEqual('Description of product one')
 
-            expect(data[1].name).toStrictEqual("Product 2");
-            expect(data[1].description).toStrictEqual("Description of product two");
-        });
+      expect(data[1].name).toStrictEqual('Product 2')
+      expect(data[1].description).toStrictEqual('Description of product two')
+    })
 
-        it("should be have a result with search", () => {
-            expect.assertions(3);
-            const rows = [{name: "Product 1", description: "Description of product one"}, {name: "Product 2", description: "Description of product two"}];
-            const data = rows.filter(objectByString("two"));
-            expect(data).toHaveLength(1);
-            expect(data[0].name).toStrictEqual("Product 2");
-            expect(data[0].description).toStrictEqual("Description of product two");
-        });
-    });
-});
+    it('should be have a result with search', () => {
+      expect.assertions(3)
+      const rows = [
+        { name: 'Product 1', description: 'Description of product one' },
+        { name: 'Product 2', description: 'Description of product two' },
+      ]
+      const data = rows.filter(objectByString('two'))
+      expect(data).toHaveLength(1)
+      expect(data[0].name).toStrictEqual('Product 2')
+      expect(data[0].description).toStrictEqual('Description of product two')
+    })
+  })
+})
