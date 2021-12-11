98d9de314394266f264dca4a14d07eba39b60f1d
Benjamin Noufel <benjamin.noufel@gmail.com>
2021-12-11 14:08:36 +0100

    :rotating_light: Fix linter for utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,14 @@
-export const entriesNotIn = (elements: string[]) => ([key]: [string, string]) => !elements.includes(key);
-export const reorderFromObject = (object: Record<string, any>, target: Record<string, any>) => Object.fromEntries(Object.keys(object).map((key: string) => [key, target[key]]));
+export const entriesNotIn =
+  (elements: string[]) =>
+  ([key]: [string, string]) =>
+    !elements.includes(key)
+export const reorderFromObject = (object: Record<string, any>, target: Record<string, any>) =>
+  Object.fromEntries(Object.keys(object).map((key: string) => [key, target[key]]))
 export const objectByString = (searchOnColunns: string | null) => (row: Record<string, any>) => {
-    if (!searchOnColunns) {
-        return row;
-    }
-    return Object.values(row).some((column: any) => String(column).toLowerCase().includes(searchOnColunns.toLowerCase()));
-};
+  if (!searchOnColunns) {
+    return row
+  }
+  return Object.values(row).some((column: any) =>
+    String(column).toLowerCase().includes(searchOnColunns.toLowerCase())
+  )
+}
